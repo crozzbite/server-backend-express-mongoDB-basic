bb9a34adf2be5b15dad535f80fc2237f2fb28841
crozzbite <jon.zavalag@gmail.com>
2025-02-17 18:29:38 -0600

    cors to de entry point of frontend 8081

--- src/server.ts
+++ src/server.ts
@@ -11,12 +11,41 @@ const app = express()//significa que aremos una app de express , es la estancia 
 conectDB()
 //leer datos
 app.use(cors({
-  origin: 'http://localhost:4000', // Permite solo solicitudes desde este origen
+  origin: 'http://localhost:8081', // Permite solo este origen
   methods: ['GET', 'POST', 'PUT', 'DELETE'], // Métodos HTTP permitidos
   credentials: true, // Permite enviar cookies o headers de autenticación
-}));
+})
+);
+
+// app.use(cors({
+//   origin: 'http://localhost:4000', // Permite solo este origen
+//   methods: ['GET', 'POST', 'PUT', 'DELETE'], // Métodos HTTP permitidos
+//   credentials: true, // Permite enviar cookies o headers de autenticación
+// })
+// );
 
 app.use(express.json())
+
+app.post('/auth/login', (req, res) => {
+  const { email, password } = req.body;
+  console.log('Datos recibidos:', { email, password });
+  res.json({ message: 'Login exitoso', user: { email } });
+});
+
+// Ruta de login
+app.post('/auth/register', (req, res) => {
+  const { handle, name, email, password } = req.body;
+  console.log('Datos recibidos:', { handle, name, email, password });
+  res.json({ message: 'Registro exitoso', user: { email } });
+});
+
+app.options('/auth/login', (req, res) => {
+  res.header('Access-Control-Allow-Origin', 'http://localhost:8081');
+  res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
+  res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+  res.send();
+});
+
 
 app.use('/', router)// use es para mapear las rutas de manera correcta 
 // puedes tener varias dependiendo de lo que necesites 
